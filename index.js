6886e1c479f9a44c9f4131c23d8c83904c783d33
Alex Oleynik <oleynik.webdev@gmail.com>
2022-06-05 23:04:17 +0300

    added getIpAddress function implementation, by default returns not internal IPv4 address as string;

--- index.js
+++ index.js
@@ -1,4 +1,21 @@
+const { networkInterfaces } = require('os');
+
 const DEFAULT_IP_ADDRESS = '0.0.0.0';
 
-// TODO: implement
-exports.getIpAddress = () => DEFAULT_IP_ADDRESS;
+module.exports.getIpAddress = (family = 'IPv4') => {
+    const netInterfaces = networkInterfaces();
+    let ipAddress = DEFAULT_IP_ADDRESS;
+
+    Object.keys(netInterfaces).forEach((netInterfacesName) => {
+        netInterfaces[netInterfacesName].some((netAddress) => {
+            // skip internal / loopback addresses
+            if (!netAddress.internal && netAddress.family === family) {
+                ipAddress = netAddress.address;
+                return true;
+            }
+            return false;
+        });
+    });
+
+    return ipAddress;
+};
