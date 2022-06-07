d9760150c2c91d958186d11d588a371cd3cd81cc
Alex Oleynik <oleynik.webdev@gmail.com>
2022-06-07 14:51:17 +0300

    getIpAddress's forEach and some replaced with for-of loops;
    eslint 'no-restricted-syntax' rule modified to remove error messages;

--- index.js
+++ index.js
@@ -13,20 +13,17 @@ const DEFAULT_IP_ADDRESS = '0.0.0.0';
  */
 module.exports.getIpAddress = (family = 'IPv4') => {
     const netInterfaces = networkInterfaces();
-    let ipAddress = DEFAULT_IP_ADDRESS;
 
-    Object.keys(netInterfaces).forEach((netInterfacesName) => {
-        netInterfaces[netInterfacesName].some((netAddress) => {
+    for (const netInterfacesName of Object.keys(netInterfaces)) {
+        for (const netAddress of netInterfaces[netInterfacesName]) {
             // skip internal / loopback addresses
             if (!netAddress.internal && netAddress.family === family) {
-                ipAddress = netAddress.address;
-                return true;
+                return netAddress.address;
             }
-            return false;
-        });
-    });
+        }
+    }
 
-    return ipAddress;
+    return DEFAULT_IP_ADDRESS;
 };
 
 /**
